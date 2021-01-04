93f422cfa0ca275d276258639a2c570aeeb38520
troysellers <tsellers@grax.com>
2021-01-04 15:28:45 +1100

    fix

--- src/test/java/com/grax/aus/AppTest.java
+++ src/test/java/com/grax/aus/AppTest.java
@@ -2,13 +2,6 @@ package com.grax.aus;
 
 import static org.junit.Assert.assertEquals;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-
 import org.junit.Test;
 
 
@@ -33,30 +26,6 @@ public class AppTest
     	assertEquals("package_usage_summary", AnalyticsRequestType.PACKAGE_USAGE_SUMMARY.getDatabaseTableName());
     	assertEquals("package_usage_log", AnalyticsRequestType.PACKAGE_USAGE_LOG.getDatabaseTableName());
     }
-    @Test
-    public void testCalendar() {
-    	Calendar endTime = Calendar.getInstance();
-    	endTime.set(Calendar.YEAR, 2021);
-    	endTime.set(Calendar.MONTH, Calendar.JANUARY);
-    	endTime.set(Calendar.DAY_OF_MONTH, 1);
-    	
-    	endTime.add(Calendar.DAY_OF_MONTH, -1);
-    	
-    	assertEquals(endTime.get(Calendar.YEAR), 2020);
-    	assertEquals(endTime.get(Calendar.MONTH), Calendar.DECEMBER);
-    	assertEquals(endTime.get(Calendar.DAY_OF_MONTH), 31);
-    	
-    	System.out.println(endTime.getTime());
-    	
-    }
-    @Test
-    public void testTimestamp() {
-    	String iso = "2021-01-02T05:02:56.452Z";
-    	Instant i = Instant.parse(iso);
-	}
-    @Test
-    public void test() {
-    	System.out.println(AnalyticsRequestType.SUBSCRIBER_SNAPSHOT.getFileName().split("\\.")[0]);
-    }
+
 }
 	
